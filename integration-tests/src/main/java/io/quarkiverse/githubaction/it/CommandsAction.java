0845e17f92984f809eb9fc305abcea2231977216
Guillaume Smet <guillaume.smet@gmail.com>
2023-11-24 15:34:01 +0100

    Handle multine outputs

--- integration-tests/src/main/java/io/quarkiverse/githubaction/it/CommandsAction.java
+++ integration-tests/src/main/java/io/quarkiverse/githubaction/it/CommandsAction.java
@@ -8,9 +8,12 @@ public class CommandsAction {
     public static final String ACTION_NAME = "CommandsAction";
     public static final String OUTPUT_KEY = "testOutputKey";
     public static final String OUTPUT_VALUE = "test output value";
+    public static final String MULTILINE_OUTPUT_KEY = "testMultilineOutputKey";
+    public static final String MULTILINE_OUTPUT_VALUE = "test\noutput\nvalue";
 
     @Action(ACTION_NAME)
     void test(Commands commands) {
         commands.setOutput(OUTPUT_KEY, OUTPUT_VALUE);
+        commands.setOutput(MULTILINE_OUTPUT_KEY, MULTILINE_OUTPUT_VALUE);
     }
 }
